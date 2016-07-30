41cc1126e164beaa4288aacee282f7082e0ebbcd
Amy Chu <amychu@Meow-2.local>
2016-07-30 18:56:40 +1200

    WIP button links

--- Source code/GamePlay.js
+++ Source code/GamePlay.js
@@ -1,8 +1,55 @@
-var questions [{question='1', answer1='This is answer 1:1', answer2='this is answer 1:2'},
-                {question='2', answer1='This is answer 2:1', answer2='this is answer 2:2'}];
+var Question1 = {
+    question : 'Question 1',
+    answers: ["Answer 1:1", "Answer 1:2"],
+    outcomes: ["outcome 1:1", "Outcome 1:2"]
+};
 
-var ClickCounterViewModel = function() {
+var Question2 = new Object();
+Question2 = {
+    question : 'Question 2',
+answers: ["Answer 2:1", "Answer 2:2"],
+outcomes: ["outcome 2:1", "Outcome 2:2"]
+};
+
+var Question3 = new Object();
+Question3 = {
+    question : 'Question 3',
+answers: ["Answer 3:1", "Answer 3:2"],
+outcomes: ["outcome 3:1", "Outcome 3:2"]
+};
+
+var questions = [Question1, Question2, Question3];
+
+var ClickCounterViewModel = function(questions) {
+    
+    this.iterator = ko.observable(0);
+    this.answer = ko.observable(0);
     this.numberOfClicks = ko.observable(0);
+    this.answerButtonView = ko.observable('hidden');
+    
+    this.outcome = ko.pureComputed(function() {
+                                   return this.iterator() + ':' + this.answer();
+                                   }, this);
+
+    this.answer1 = ko.pureComputed(function () {
+                                   return questions[this.iterator()].answers[0];
+                                   }, this);
+    
+    this.answer2 = ko.pureComputed(function() {
+                                   return questions[this.iterator()].answers[1]
+                                   }, this);
+    
+    this.registerClick1 = function() {
+        this.answer(1);
+        this.iterator(this.iterator() + 1);
+        this.numberOfClicks(this.numberOfClicks() + 1);
+    }
+    
+    this.registerClick2 = function() {
+        this.answer(2);
+        this.iterator(this.iterator() + 1);
+        this.numberOfClicks(this.numberOfClicks() + 1);
+    }
     
     this.registerClick = function() {
         this.numberOfClicks(this.numberOfClicks() + 1);
@@ -15,15 +62,15 @@ var ClickCounterViewModel = function() {
     };
     
     this.registerPlay = function() {
-        this.registerPlay('button')
+        this.registerPlay('hidden')
     };
     
-    this.registerAnswer1 = 'this is answer 1';
-    
-    this.registerAnswer1 = 'this is answer 2';
+    this.registerPause = function() {
+        this.registerPause('hidden')
+    };
     
     this.hasClickedTooManyTimes = ko.pureComputed(function() {
-                                                  return this.numberOfClicks() >= 3;
+                                                  return this.numberOfClicks() >= 1;
                                                   }, this);
 };
 
